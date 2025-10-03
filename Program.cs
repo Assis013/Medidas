c502454c1af6e6c36d8e88e3d4c6cecd5a7d7829
Leticia <leticiagabriely629@gmail.com>
2025-10-03 20:43:26 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿double m, cm, km;
+
+Console.Write("Entre com a media (em metros):");
+m = Convert.ToDouble(Console.ReadLine());
+
+cm = m * 100;
+m = 150;
+km = m / 100;
+
+Console.WriteLine("/n --- Equivalence ---");
+Console.WriteLine($"{cm} cm");
+Console.WriteLine($"{m} m");
+Console.WriteLine($"{km} km");
